242081bcf33cb783aa9ffaf474ed982c944331cb
yaainouc <yaainouc@student.42.fr>
2024-03-29 17:36:24 +0100

    Push main

--- src/Channel.d
+++
@@ -1,2 +0,0 @@
-src/Channel.o: src/Channel.cpp src/../include/Channel.hpp \
- src/../include/User.hpp src/../include/Message.hpp
--- src/main.d
+++
@@ -1,3 +0,0 @@
-src/main.o: src/main.cpp src/../include/main.hpp src/../include/User.hpp \
- src/../include/Server.hpp src/../include/Server_msg.hpp \
- src/../include/Channel.hpp src/../include/Message.hpp
