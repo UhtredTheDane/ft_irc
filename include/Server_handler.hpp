f130538141d42d56b98c8cc4d3596939fd128d89
agengemb <gengembre.alexis@gmail.com>
2024-04-04 17:12:50 +0200

    handler switch ok

--- include/Server_handler.hpp
+++ include/Server_handler.hpp
@@ -6,7 +6,7 @@
 /*   By: agengemb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 16:07:32 by agengemb          #+#    #+#             */
-/*   Updated: 2024/04/03 16:43:32 by agengemb         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:11:31 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,12 @@ class Server_handler
 
 	public:
 
-        Server_handler(void);
+		Server_handler(void);
 		User* add_user(int fd_client);
 		void delete_user(int fd_client);
 		Channel* add_channel(std::string name, User* user);
 		std::map<std::string, Channel*> get_channels(void);
-        	std::map<int, User*> get_users(void);
+		std::map<int, User*> get_users(void);
 		void request_handler(int client_socket, std::string &request);
 		bool is_on_serv(std::string& nickname);
 		void processing_request(User* user, std::string& request);
@@ -48,10 +48,10 @@ class Server_handler
 		void pong_request(User* user);
 
 	private:
-	
-        Server_msg msg;
-        std::vector<std::string> split_line;
-        std::string request_types[10];
+
+		Server_msg msg;
+		std::vector<std::string> split_line;
+		std::string request_types[10];
 		void (Server_handler::*requests_ptr[10])(User*);
 		std::map<int, User*> users_map;
 		std::map<std::string, Channel*> channels;
--- src/Server_handler.cpp
+++ src/Server_handler.cpp
@@ -6,7 +6,7 @@
 /*   By: agengemb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 16:07:38 by agengemb          #+#    #+#             */
-/*   Updated: 2024/04/03 17:59:24 by agengemb         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:11:40 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 Server_handler::Server_handler(void)
 {
-    	msg = Server_msg();
-	request_types[0] = "CAP LS";
+	msg = Server_msg();
+	request_types[0] = "CAP";
 	request_types[1] = "PASS";
 	request_types[2] = "NICK";
 	request_types[3] = "USER";
-    	request_types[4] = "PING";
+	request_types[4] = "PING";
 	request_types[5] = "JOIN";
 	request_types[6] = "MODE";
 	request_types[7] = "KICK"; 
@@ -68,9 +68,8 @@ Channel* Server_handler::add_channel(std::string name, User* user)
 }
 void Server_handler::capls_request(User* user)
 {
-	if (user->get_isRegistered() == 0)
+	if (!split_line[1].compare("LS") && user->get_isRegistered() == 0)
 	{
-		std::cout << "Salut\n";
 		user->set_isRegistered(1);
 	}
 }
@@ -80,7 +79,6 @@ void Server_handler::pass_request(User* user)
 	std::cout << user->get_isRegistered() << std::endl;
 	if(user->get_isRegistered() == 1)
 	{
-		std::cout << "toto\n";
 		std::cout << "PASS valide\n";
 	}
 }
@@ -89,10 +87,9 @@ void Server_handler::nick_request(User* user)
 {
 	if(user->get_isRegistered() == 1)
 	{
-		std::cout << "coco\n";
 		while (is_on_serv(split_line[1]))
-				split_line[1] += "_";
-			user->set_nickname(split_line[1]);
+			split_line[1] += "_";
+		user->set_nickname(split_line[1]);
 	}
 }
 
@@ -106,7 +103,6 @@ void Server_handler::user_request(User* user)
 		user->set_realname(split_line[3]);
 		user->set_identifier();
 		// user.show_userinfo(user);
-		std::cout << "popo\n";
 		msg.welcome_msg(user);
 		msg.yourhost_msg(user);
 		msg.created_msg(user);
@@ -122,88 +118,88 @@ void Server_handler::pong_request(User* user)
 
 void Server_handler::join_request(User* user)
 {
-		try
-		{	
-			std::cout << user->get_username() << std::endl;
-			Channel* current_chan = get_channels().at(split_line[1]);
-			current_chan->add_user(user);
-			msg.join_msg(user, current_chan);
+	try
+	{	
+		std::cout << user->get_username() << std::endl;
+		Channel* current_chan = get_channels().at(split_line[1]);
+		current_chan->add_user(user);
+		msg.join_msg(user, current_chan);
 
-			std::string join_msg;
-			join_msg += ":" + user->get_nickname() + "!" + user->get_nickname() + "@localhost JOIN :" + current_chan->get_name();
-			join_msg += "\r\n";
-			for (std::vector<User*>::iterator it = current_chan->get_users()->begin(); it != current_chan->get_users()->end(); ++it)
-			{
-				if (user != *it)
-					send((*it)->get_socket(), join_msg.c_str(), join_msg.length(), 0);
-			}
-
-		}
-		catch (std::out_of_range& oor)
+		std::string join_msg;
+		join_msg += ":" + user->get_nickname() + "!" + user->get_nickname() + "@localhost JOIN :" + current_chan->get_name();
+		join_msg += "\r\n";
+		for (std::vector<User*>::iterator it = current_chan->get_users()->begin(); it != current_chan->get_users()->end(); ++it)
 		{
-			msg.join_msg(user, add_channel(split_line[1], user));
+			if (user != *it)
+				send((*it)->get_socket(), join_msg.c_str(), join_msg.length(), 0);
 		}
+
+	}
+	catch (std::out_of_range& oor)
+	{
+		msg.join_msg(user, add_channel(split_line[1], user));
+	}
 }
 
 void Server_handler::mode_request(User* user)
 {
-		try
-		{
-			Channel* target_chan = get_channels().at(split_line[1]);
-			msg.mode_msg(user, target_chan);
-		}
-		catch (std::out_of_range& oor)
-		{
-		}
+	try
+	{
+		Channel* target_chan = get_channels().at(split_line[1]);
+		msg.mode_msg(user, target_chan);
+	}
+	catch (std::out_of_range& oor)
+	{
+	}
 }
 
 void Server_handler::kick_request(User* user)
 {
-		Channel *curent_chan = get_channels().at(split_line[1]);
-		for (std::vector<User*>::iterator it = curent_chan->get_admins()->begin(); it != curent_chan->get_admins()->end();)
+	Channel *curent_chan = get_channels().at(split_line[1]);
+	for (std::vector<User*>::iterator it = curent_chan->get_admins()->begin(); it != curent_chan->get_admins()->end();)
+	{
+		if(user == *it)
 		{
-			if(user == *it)
+			std::string k_msg;
+			for (std::vector<User*>::iterator it2 = curent_chan->get_users()->begin(); it2 != curent_chan->get_users()->end();)
 			{
-				std::string k_msg;
-				for (std::vector<User*>::iterator it2 = curent_chan->get_users()->begin(); it2 != curent_chan->get_users()->end();)
-					{
-						if((*it2)->get_nickname() == split_line[2])
-							{
-								k_msg = ":" + user->get_identifier() + " KICK " + curent_chan->get_name() + " " + split_line[2] + " " + split_line[3] + "\r\n";
-								send((*it2)->get_socket(), k_msg.c_str(), k_msg.length(), 0);
-								break;
-							}
-						it2++;
-					}
+				if((*it2)->get_nickname() == split_line[2])
+				{
+					k_msg = ":" + user->get_identifier() + " KICK " + curent_chan->get_name() + " " + split_line[2] + " " + split_line[3] + "\r\n";
+					send((*it2)->get_socket(), k_msg.c_str(), k_msg.length(), 0);
+					break;
+				}
+				it2++;
 			}
-			it++;
 		}
+		it++;
+	}
 }
 
 void Server_handler::privmsg_request(User* user)
 {
 	try
+	{
+		if(split_line[1][0] == '#')
 		{
-			if(split_line[1][0] == '#')
-			{
-				Channel *curent_chan = get_channels().at(split_line[1]); 
-				Message msg(split_line[2], user);
-				curent_chan->add_message(&msg);
+			Channel *curent_chan = get_channels().at(split_line[1]); 
+			Message msg(split_line[2], user);
+			curent_chan->add_message(&msg);
 			std::string c_msg;
 			for (std::vector<User*>::iterator it = curent_chan->get_users()->begin(); it != curent_chan->get_users()->end();)
 			{
 				if(*it != user)
 				{
-				c_msg = ":" + user->get_identifier() + " PRIVMSG " + curent_chan->get_name()+ " " + msg.get_msg() + "\r\n";
-				std::cout << c_msg << std::endl;
-				send((*it)->get_socket(), c_msg.c_str(), c_msg.length(), 0);
+					c_msg = ":" + user->get_identifier() + " PRIVMSG " + curent_chan->get_name()+ " " + msg.get_msg() + "\r\n";
+					std::cout << c_msg << std::endl;
+					send((*it)->get_socket(), c_msg.c_str(), c_msg.length(), 0);
 				}
-			it++;
+				it++;
 			}
-			}
-			else
-			{
-                std::map<int, User*> users_map = get_users();
+		}
+		else
+		{
+			std::map<int, User*> users_map = get_users();
 			for (std::map<int, User*>::iterator it = users_map.begin(); it != users_map.end(); ++it)
 			{
 				if (it->second->get_nickname() == split_line[1])
@@ -215,24 +211,24 @@ void Server_handler::privmsg_request(User* user)
 					break;
 				}
 			}
-			}
 		}
-		catch (std::out_of_range& oor)
-		{
-		}
+	}
+	catch (std::out_of_range& oor)
+	{
+	}
 }
 
 void Server_handler::part_request(User* user)
 {
-		try
-		{
-			Channel *current_chan = get_channels().at(split_line[1]); 
-			msg.leave_msg(user, current_chan);
-			current_chan->delete_user(user);
-		}
-		catch (std::out_of_range& oor)
-		{
-		}
+	try
+	{
+		Channel *current_chan = get_channels().at(split_line[1]); 
+		msg.leave_msg(user, current_chan);
+		current_chan->delete_user(user);
+	}
+	catch (std::out_of_range& oor)
+	{
+	}
 }
 
 void Server_handler::processing_request(User* user, std::string& request)
@@ -244,7 +240,7 @@ void Server_handler::processing_request(User* user, std::string& request)
 		split_line.push_back(word);
 	for (int i = 0; i < 10; ++i)
 	{
-		if (!request.compare(request_types[i]))
+		if (!split_line[0].compare(request_types[i]))
 		{
 			(this->*requests_ptr[i])(user);
 			break;
@@ -257,12 +253,12 @@ bool Server_handler::is_on_serv(std::string& nickname)
 {
 	try
 	{
-    std::map<int, User*> users_map = get_users();
-	for (std::map<int, User*>::iterator it = users_map.begin(); it != users_map.end(); ++it)
-	{
-		if (it->second->get_nickname() == nickname)
-			return (true);
-	}
+		std::map<int, User*> users_map = get_users();
+		for (std::map<int, User*>::iterator it = users_map.begin(); it != users_map.end(); ++it)
+		{
+			if (it->second->get_nickname() == nickname)
+				return (true);
+		}
 	}
 	catch (std::out_of_range& oor)
 	{
@@ -278,28 +274,27 @@ void Server_handler::request_handler(int client_socket, std::string& request)
 	std::string token;
 	size_t delim_pos;
 
-    	std::map<int, User*> users_map = get_users();
+	std::map<int, User*> users_map = get_users();
 	try
 	{
 		user = users_map.at(client_socket);
 		if (request.find(delimiter, 0) != std::string::npos)
 		{
-		user->buffer += request;
-		while((delim_pos = user->buffer.find(delimiter)) != std::string::npos)
-		{
-			token = user->buffer.substr(0, delim_pos);
-			std::cout << token << std::endl;
-			processing_request(user, token);
-			user->buffer.erase(0, delim_pos + delimiter.length());
+			user->buffer += request;
+			while((delim_pos = user->buffer.find(delimiter)) != std::string::npos)
+			{
+				token = user->buffer.substr(0, delim_pos);
+				std::cout << token << std::endl;
+				processing_request(user, token);
+				user->buffer.erase(0, delim_pos + delimiter.length());
+			}
 		}
-	}
-	else
-	{
-		user->buffer += request;
-	}
+		else
+		{
+			user->buffer += request;
+		}
 	}
 	catch(std::out_of_range& oor)
 	{
 	}
-
 }
