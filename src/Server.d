47c07bd636593c753c1cc9d861da95c863418ab5
Yanis AINOUCHE <yaainouc@paul-f4Br4s5.clusters.42paris.fr>
2024-04-26 15:28:25 +0200

    ERROR FIX, ADD COLORS

--- src/Server.d
+++
@@ -1,4 +0,0 @@
-src/Server.o: src/Server.cpp src/../include/Server.hpp \
-  src/../include/Server_handler.hpp src/../include/User.hpp \
-  src/../include/Channel.hpp src/../include/Message.hpp \
-  src/../include/Server_msg.hpp
